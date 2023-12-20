db856280b8a4175a441d112c86aacaa1f1701ff6
Anil <anilkumar.mittana@gmail.com>
2023-12-21 01:26:42 +0530

    added dependcy

--- 2.Git-Terraform-Azure/Random_Password.tf
+++ 2.Git-Terraform-Azure/Random_Password.tf
@@ -2,14 +2,17 @@ resource "random_password" "password3" {
   length           = 16
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
+  depends_on = [ azurerm_virtual_network.Anil-vnet1 ]
 }
 resource "random_password" "password2" {
   length           = 16
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
+  depends_on = [ random_password.password3 ]
 }
 resource "random_password" "password1" {
   length           = 16
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
+  depends_on = [ random_password.password2 ]
 }
