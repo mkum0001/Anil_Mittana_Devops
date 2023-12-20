589d3046db88cbe5635f8187aab5fc8b27189115
Anil <anilkumar.mittana@gmail.com>
2023-12-21 01:24:36 +0530

    added 3 passwords

--- 2.Git-Terraform-Azure/Random_Password.tf
+++ 2.Git-Terraform-Azure/Random_Password.tf
@@ -1,4 +1,14 @@
-resource "random_password" "password" {
+resource "random_password" "password3" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+resource "random_password" "password2" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+resource "random_password" "password1" {
   length           = 16
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
