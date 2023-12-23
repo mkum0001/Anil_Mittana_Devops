792cc2c213cabda82dbe4868fcfddce56240df45
Anil <anilkumar.mittana@gmail.com>
2023-12-23 07:21:07 +0530

    added nsg

---
+++ 2.Git-Terraform-Azure/NSG.tf
@@ -0,0 +1,21 @@
+resource "azurerm_network_security_group" "Anil-nsg" {
+  name                = "Anil-nsg"
+  location            = azurerm_resource_group.Anil-rg1.location
+  resource_group_name = azurerm_resource_group.Anil-rg1.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
