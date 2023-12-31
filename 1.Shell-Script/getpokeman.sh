2ae7c2776ebe7f41c54ae7f341997996209401e0
Anil.Mittana <anilkumar.mittana@gmail.com>
2023-12-31 07:41:09 +0530

    tips

--- 1.Shell-Script/getpokeman.sh
+++ 1.Shell-Script/getpokeman.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 ### define start and stop ranges
-startrange=10
-stoprange=30
+startrange=$1
+stoprange=$2
 ## List the names from the pokemanAPI
-curl -s "https://pokeapi.co/api/v2/pokemon?offset=${startrange}&limit=${stoprange}"  | jq .results[].name -r
+curl -s "https://pokeapi.co/api/v2/pokemon?offset=${startrange}&limit=${stoprange}" \
+| jq .results[].name -r
 ## count the names
 count=$(curl -s "https://pokeapi.co/api/v2/pokemon?offset=${startrange}&limit=${stoprange}"  | jq .results[].name -r| wc -l)
 ##print the count
