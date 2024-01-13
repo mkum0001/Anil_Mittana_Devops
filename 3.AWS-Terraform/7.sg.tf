861ef6b05a63ca344ffa6478e473f7f108fbc0e0
Anil.Mittana <anilkumar.mittana@gmail.com>
2024-01-13 06:21:56 +0530

    added SG

---
+++ 3.AWS-Terraform/7.sg.tf
@@ -0,0 +1,48 @@
+data "http" "mydesktopip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.mydesktopip.response_body)}/32"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+#   lifecycle {
+#     ignore_changes = [ingress]
+#   }
+}
